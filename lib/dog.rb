0c10ff69cc55fdc665c29318fee72c89cc2c9c41
Aftab Sidhu <Aftab180@gmail.com>
2022-06-12 18:08:25 -0400

    commit tree bb69bf6e1e6a86ec13b255b2fb9ca7d7845d9424

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    def name
+        @this_dogs_name
+    end
 
 end
+
+# fido = Dog.new
+# fido.this_dogs_name('Puppy')
+# fido.get_this_dog_name
